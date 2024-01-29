0de468d132e2d0f9da7c2b9bf00a1b30e383b586
RGH84 <jhmkemppainen16@gmail.com>
2024-01-29 15:23:15 +0200

    Create register, login and logout
    Created login, logout and register functions. The program has also
    been divided into modules and new pages have been added to the
    templates folder.

---
+++ routes.py
@@ -0,0 +1,45 @@
+from db import db
+from app import app
+import visits
+from flask import redirect, render_template, request, session
+import users
+
+@app.route("/")
+def index():
+    visits.add_visit()
+    counter = visits.get_counter()
+    return render_template("index.html", counter=counter)
+ 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            session["username"] = username
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Väärä tunnus tai salasana")
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Salasanat eroavat")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Rekisteröinti ei onnistunut")
+
